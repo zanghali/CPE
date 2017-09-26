2cfbebfb2833c394f9e5d86460584a1a675578e0
Ghali Zangui <zanghali@hotmail.com>
2017-09-26 11:10:18 +0200

    First commit maven tp

---
+++ maven/td-maven/src/main/java/fr/cpe/App.java
@@ -0,0 +1,17 @@
+package fr.cpe;
+
+import org.joda.time.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	DateTime now = new DateTime();
+    	
+        System.out.println( "Hello World!" );
+    }
+}
